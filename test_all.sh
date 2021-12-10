01c723440b8d2a06243a4fe575930ea02324828c
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-12-10 21:56:32 +0530

    Run mypy in strict mode

--- test_all.sh
+++ test_all.sh
@@ -1,2 +1,2 @@
 find . -maxdepth 1 -type d -name 'day_??' | xargs -i pytest {}/main.py
-find . -maxdepth 1 -type d -name 'day_??' | xargs -i mypy {}/main.py
+find . -maxdepth 1 -type d -name 'day_??' | xargs -i mypy --strict {}/main.py
